ecf54f891297a6aca996bb8a2226485806d99758
Nathan Patterson <imnathanpatterson@gmail.com>
2025-02-25 20:14:29 -0500

    server up and functional

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const tRoutes = require("./routes/T-Routes")
+const errorHandler = require("./middleware/errorHandler");
 
 dotenv.config();
 
@@ -9,6 +10,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use("/api", tRoutes)
+app.use(errorHandler);
 
 app.get("/", (req, res) => {
     res.send("TSync API running...");
