61caadc833d0e73883d3ee6bfa85c0caa7a50eb5
Nathan Patterson <imnathanpatterson@gmail.com>
2025-02-25 19:48:14 -0500

    simple server architecture implemented

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("TSync API running...");
+});
+
+app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
